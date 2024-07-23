6399b8980d796be2d00f655cdfc9401b3e4004fe
Blyudin Andrey <Andrey.Blyudin@infotecs.ru>
2024-07-23 18:54:25 +0300

    test polymorpishm with smart pointers

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,58 @@
+#include <fstream>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 
-#include "my_zip.h"
-
-
-int main( int argc, char** argv )
+class Base
 {
-     ZipManager zipManager;
+public:
+     virtual ~Base() = default;
+     int base_1;
+};
 
-     if ( !zipManager.createZipArchiveFromDirectory( "/home/andrey/Projects/testBoost/to/da.zip", "/home/andrey/Projects/testBoost/from" ) )
-     {
-          std::cout << "Error" << std::endl;
-     }
-     return 0;
+class Derived : public Base
+{
+public:
+     int derived_1;
+};
+
+std::shared_ptr< Base > Make()
+{
+     auto elem = std::make_shared< Derived >();
+     std::cout << "Make() elem = " << typeid( elem ).name() << std::endl;
+     return elem;
+}
+
+void fun( std::shared_ptr< Base >& elem )
+{
+     std::cout << "fun() elem typeid.name = " << typeid( elem ).name() << std::endl;
+}
+
+// Base* Make()
+// {
+//      auto elem = new Derived();
+//      std::cout << "Make() elem = " << typeid( *elem ).name() << std::endl;
+//      return elem;
+// }
+
+// void fun( Base* elem )
+// {
+//      std::cout << "fun() elem typeid.name = " << typeid( *elem ).name() << std::endl;
+// }
+
+int main()
+{
+     // auto elem = Make();
+     auto elem = std::make_shared< Derived >();
+     std::cout << "main() elem typeid.name = " << typeid( elem ).name() << std::endl;
+     fun( elem );
+     std::cout << "main() elem typeid.name = " << typeid( elem ).name() << std::endl;
+
+
+     // auto d = Derived();
+     // std::cout << "d typeid.name = " << typeid( d ).name() << std::endl;
+
+     // auto b = d;
+     // std::cout << "b typeid.name = " << typeid( b ).name() << std::endl;
 }
